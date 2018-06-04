72555e53e1e36342c8a85096b369574a23bcdf0c
bergren2 <dan@heymrbass.com>
2018-06-03 22:02:25 -0500

    update with blocker example, add setup

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 Firefox Extension to help quiet the noise.
 
+## Setup
+
+    $ yarn install
+
+## Development
+
+    $ yarn run start:firefox
+
+If you need to pass in a specific browser, you can do so as such:
+
+    $ yarn run start:firefox --firefox=firefoxdeveloperedition
+
 ## License
 
 See LICENSE for details.
--- dist/background.js
+++ dist/background.js
@@ -1,8 +1,14 @@
-function logURL(requestDetails) {
-  console.log("Loading: " + requestDetails.url);
+var pattern = "https://mdn.mozillademos.org/*";
+
+function redirect(requestDetails) {
+  console.log("Redirecting: " + requestDetails.url);
+  return {
+    redirectUrl: "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif"
+  };
 }
 
 browser.webRequest.onBeforeRequest.addListener(
-  logURL,
-  {urls: ["<all_urls>"]}
+  redirect,
+  {urls:[pattern], types:["image"]},
+  ["blocking"]
 );
